2b88a1bfca145aeb267246fd4f25fe135ca13369
alessiadonati2000 <alessia.donati1@edu.unifi.it>
2025-07-16 10:31:48 +0200

    Association.java

--- src/main/java/Association.java
+++ src/main/java/Association.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class Association {
+abstract class Association {
     protected List<User> users;
     protected List<Server> servers;
     protected int[][] associationMatrix;
